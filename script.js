d161961fa9924e8a8efab51a78ef4fa9a7ff2379
Maren Lilleberre <marenlilleberre@gmail.com>
2021-07-06 11:15:34 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+const dadJokeBtn = document.getElementById('dadJokeBtn')
+
+generateDadJoke();
+
+function generateDadJoke(){
+    const config ={
+        headers: {
+            Accept:'application/json',
+        }, 
+    }
+
+    fetch('https://icanhazdadjoke.com', config)
+    .then((response) => response.json())
+    .then((data) => jokeEl.innerHTML = data.joke);
+    
+
+    
+}
+/* function generateJoke(){
+    const config ={
+        headers:{
+            Accept:'application/json',
+        },
+    }
+    fetch('https://api.jokes.one/joke/random', config)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+}
+generateJoke(); */
+
+
+
+
+dadjokeBtn.addEventListener('click',() =>{
+    generateDadJoke()
+})
